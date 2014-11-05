17c0a5a4b3afe5de6f5db6ec3cde99aebdcabf35
David Brough <david.brough.0416@gmail.com>
2014-11-05 13:12:22 -0500

    Update ImageDimenionReducer class and test
    Update ImageDimensionReducer to take in a folder path. Update test
    file accordingly.

--- python_scripts/image_dimension_reducer.py
+++ python_scripts/image_dimension_reducer.py
@@ -1,8 +1,6 @@
 import numpy as np
 import os
-import matplotlib.pylab as plt
-from PIL import Image
-from dropbox_path import dropbox_path
+from libtiff import TIFF
 
 
 class ImageDimensionReducer(object):
@@ -13,6 +11,9 @@ class ImageDimensionReducer(object):
     """
 
     def __init__(self, model=None):
+        '''
+        Create an instance of ImageDimensionReducer.
+        '''
         self.model = model
         if self.model is None:
             raise RuntimeError("Must specify model")
@@ -26,25 +27,21 @@ class ImageDimensionReducer(object):
             raise RuntimeError("Must specify data_name")
         if data_file is None:
             raise RuntimeError("Must specify data_file")
-        sample_data = self._get_data(file_name)
+        sample_data = self._get_data(data_name)
         self.data[data_name] = sample_data
 
     def load_data(self, folder, file_type):
-        directory_path = os.path.join(dropbox_path(), folder)
         k = 0
-        for i in os.listdir(directory_path):
-            if i.endswith(file_type):
-                script_dir = os.path.dirname(os.getcwd())
-                im = Image.open(os.path.join(directory_path, i))
-                im = im.convert("L")
-                imarray = np.array(im)
-                if imarray.shape == (2832, 4256) and k < 10:
-                    if k == 0:
-                        raw_data = imarray[None]
-                        k = 1
-                    else:
-                        raw_data = np.concatenate((raw_data, imarray[None]))
-                        k = k + 1
+        file_names = sorted(os.listdir(folder))
+        for file_name in file_names:
+            if file_name.endswith(file_type):
+                im = TIFF.open(os.path.join(folder, file_name))
+                imarray = np.log(im.read_image() + 0.1)
+                if k == 0:
+                    raw_data = imarray[None]
+                    k = 1
+                else:
+                    raw_data = np.concatenate((raw_data, imarray[None]))
         data = self._format_data(raw_data)
         self.data[folder] = data
         self.n_microstructures = self.n_microstructures + 1
@@ -72,5 +69,4 @@ class ImageDimensionReducer(object):
         self.data = {}
         self.preped_data = None
         self.n_t_steps = 0
-        self.n_microstructures =0
-
+        self.n_microstructures = 0
--- python_scripts/test.py
+++ python_scripts/test.py
@@ -1,21 +1,10 @@
 from sklearn.decomposition import PCA
-from sklearn.manifold import MDS
 from image_dimension_reducer import ImageDimensionReducer
 from pymks.tools import draw_PCA
 
-model = PCA(n_components=2)
+model = PCA(n_components=3)
 IDR = ImageDimensionReducer(model=model)
-IDR.load_data('Brough Family Pictures', '.JPG')
+IDR.load_data('/home/david/matials_data_temp/', '.tif')
 IDR.prep_data()
 X_PCA = IDR.fit_transform()
-
-model = MDS(n_components=2)
-IDR.model = model
-X_MDS = IDR.fit_transform()
-
-draw_PCA(X_PCA, 2)
-draw_PCA(X_MDS, 2)
-
-
-
-
+draw_PCA(X_PCA, 1)
