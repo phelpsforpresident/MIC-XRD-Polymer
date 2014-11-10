33d67ee0a804aea8d95d8b896d8384918d5aa381
David Brough <david.brough.0416@gmail.com>
2014-11-10 15:30:13 -0500

    Changed PCA to RandomizedPCA
    Changed PCA to RandomizedPCA and add print statements with time.

--- python_scripts/test.py
+++ python_scripts/test.py
@@ -1,10 +1,17 @@
-from sklearn.decomposition import PCA
+from sklearn.decomposition import RandomizedPCA
 from image_dimension_reducer import ImageDimensionReducer
 from pymks.tools import draw_PCA
+import time
 
-model = PCA(n_components=3)
+model = RandomizedPCA(n_components=3)
 IDR = ImageDimensionReducer(model=model)
+t_start = time.time()
 IDR.load_data('/home/david/matials_data_temp/', '.tif')
+print 'Loaded Data in', time.time() - t_start, 'sec'
+t_start = time.time()
 IDR.prep_data()
+print 'Prep Data in', time.time() - t_start, 'sec'
+t_start = time.time()
 X_PCA = IDR.fit_transform()
+print 'Fit PCA in ', time.time() - t_start, 'sec'
 draw_PCA(X_PCA, 1)
