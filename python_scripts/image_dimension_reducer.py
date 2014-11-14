f72825df772716c7ae759a567c7273e8770bafd4
David Brough <david.brough.0416@gmail.com>
2014-11-14 13:03:28 -0500

    Revamp ImageDimensionReducer
    Revamped ImageDimensionReducer. Updated documentation. Improved load
    file function. Added save_to_json method, but still needs to be
    implemented.

--- python_scripts/image_dimension_reducer.py
+++ python_scripts/image_dimension_reducer.py
@@ -4,73 +4,120 @@ import skimage.io as skio
 
 
 class ImageDimensionReducer(object):
+
     """
-    This `ImagePCA` uses Principle Component Analysis (PCA) to objectively
-    find the differences between images.
+    This `ImageDimensionReducer` uses takes in a dimensionality reduction
+    class from sklearn, and creates a reduced representation of the images.
     """
 
     def __init__(self, model=None):
         '''
         Create an instance of ImageDimensionReducer.
+
+        Args:
+            model: dimensionality reduction class from sklearn. The class must
+                have a 'fit_transform' method.
         '''
         self.model = model
         if self.model is None:
             raise RuntimeError("Must specify model")
         self.data = {}
-        self.preped_data = None
-        self.n_t_steps = 0
-        self.n_microstructures = 0
+        self.reduced_data = {}
+        self.n_images = 0
+        self.n_arrays = 0
+        self.n_datasets = 0
 
-    def Load_data(self, data_file=None, data_name=None):
-        if data_name is None:
-            raise RuntimeError("Must specify data_name")
-        if data_file is None:
-            raise RuntimeError("Must specify data_file")
-        sample_data = self._get_data(data_name)
-        self.data[data_name] = sample_data
+    def load_images(self, directory_path=None, file_type=None):
+        '''
+        load data from a directory
 
-    def load_data(self, folder, file_type):
-        k = 0
-        file_names = sorted(os.listdir(folder))
-        for file_name in file_names:
-            if k == (k + 1) / 2:
-                if file_name.endswith(file_type):
-                    imarray = skio.imread(os.path.join(folder, file_name))
-                    imarray = np.log(imarray + 0.1)
-                    imarray = imarray / np.max(imarray)
-                    imarray = imarray[:600, 400:]
-                    if k == 0:
-                        raw_data = imarray[None]
-                        k = k + 1
-                    else:
-                        raw_data = np.concatenate((raw_data, imarray[None]))
-            else:
-                k = k + 1
-        data = self._format_data(raw_data)
-        self.data[folder] = data
-        self.n_microstructures = self.n_microstructures + 1
+        Args:
+            directory_path: path to directory containing image files.
+            file_type: provides an option to specify the type of file to be
+                loaded.
+        '''
+        if directory_path is None:
+            raise RuntimeError("Must specify directory_path")
+        if file_type is None:
+            file_type = '.*'
+        self._load_images(directory_path, file_type)
 
-    def prep_data(self):
+    def _load_images(self, directory, file_type):
+        '''
+        Helper funciton to load image files.
+        '''
+        file_names = sorted(os.listdir(directory))
+        im = skio.imread(os.path.join(directory, file_names[0]))
+        raw_data = im[None]
+        self.data[file_names[0]] = raw_data
+        self.n_datasets = self.n_datasets + 1
+        for file_name in file_names[1:]:
+            if file_name.endswith(file_type):
+                im = skio.imread(os.path.join(directory, file_name))
+                raw_data = np.concatenate((raw_data, im[None]))
+                self.n_images = self.n_images + 1
+        self.data[file_name] = raw_data
+        self.n_datasets = self.n_datasets + self.n_images
+
+    def _manipulate_data(self, im):
+        '''
+        Helper function to manipulate arrays representing images.
+        '''
+        im_log = np.log(im + 1)
+        im_log_norm = im_log / np.max(im_log)
+        return im_log_norm[:600, 400:]
+
+    def _prep_data(self):
+        '''
+        Helper function that pulls data out of dictionary and into an array.
+        '''
         size = self.data[self.data.keys()[0]].shape
-        l = len(self.data)
-        self.n_t_steps = size[0]
-        size = (l * self.n_t_steps,) + size[1:]
+        size = (self.n_datasets,) + (np.prod(size[1:]),)
         preped_data = np.zeros(size)
-        i = 0
         for key, value in self.data.iteritems():
-            preped_data[i * self.n_t_steps:(i + 1) * self.n_t_steps] = value
-        self.preped_data = preped_data
+            formated_data = self._format_data(value)
+            preped_data[:self.n_datasets] = formated_data
+        return preped_data
 
     def _format_data(self, raw_data):
+        '''
+        Changes the size of the data to mee
+        '''
         size = np.array(raw_data.shape)
         new_size = (size[0], np.prod(size[1:]))
         return raw_data.reshape(new_size)
 
     def fit_transform(self):
-        return self.model.fit_transform(self.preped_data)
+        '''
+        This function leverages the model's fit_transform function to reduce
+        the number of dimensions.
+        '''
+        preped_data = self._prep_data()
+        self.reduced_data = self.model.fit_transform(preped_data)
+        return self.reduced_data
 
     def remove_data(self):
+        '''
+        removes all data from ImageDimensionReducer
+        '''
         self.data = {}
-        self.preped_data = None
-        self.n_t_steps = 0
-        self.n_microstructures = 0
+        self.reduced_data = {}
+        self.n_images = 0
+        self.n_arrays = 0
+        self.n_datasets = 0
+
+    def save_to_json(self):
+        '''
+        Saves data to json file.
+        '''
+        if self.data == {}:
+            raise RuntimeError("No data to save.")
+        raise NotImplementedError("Not needed yet.")
+
+    def load_array(self, X, name=None):
+        '''
+        Loads an array as data.
+        '''
+        if name is None:
+            name = str(self.n_datasets + 1)
+        self.data[name] = X[None]
