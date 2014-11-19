62ab08b19947665f927a08d30333cd94948de640
David Brough <david.brough.0416@gmail.com>
2014-11-18 21:23:04 -0500

    Add ImageManipulatorDimensionReducer
    Add ImageManipulatorDimensionReducer. This class in
    ImageDimensionReducer, but allows for image data to be manipulated
    before dimensionality reduction is preformed on it. In this case
    class the images can be sliced and then the log of data is then
    normalized.

--- python_scripts/test.py
+++ python_scripts/test.py
@@ -1,32 +1,36 @@
 import time
 from sklearn.decomposition import KernelPCA
-from image_dimension_reducer import ImageDimensionReducer
+from image_manipulator_dimension_reducer \
+    import ImageManipulatorDimensionReducer
 from pymks.tools import draw_PCA
 
 reducer = KernelPCA(n_components=3)
-IDR = ImageDimensionReducer(reducer=reducer)
+IMDR = ImageManipulatorDimensionReducer(reducer=reducer)
 
 t_start = time.time()
-#IDR.load_images('/Users/abhiramkannan/Documents/SAXS_for_DB/', '.tif')
-IDR.load_images(directory_path='/home/david/Pictures/SAXS_for_DB/',
-                file_type='.tif', dataset_name='SAX_2')
-print 'Loaded Data in', time.time() - t_start, 'sec'
+#IMDR.load_images('/Users/abhiramkannan/Documents/SAXS_for_DB/', '.tif')
+IMDR.load_images(directory_path='/home/david/Pictures/SAXS_for_DB/',
+                 file_type='.tif', dataset_name='SAX_2')
+print 'Loaded data in', time.time() - t_start, 'secs'
+
+t_start = time.time()
+IMDR.manipulate(new_slice=(slice(None), slice(360, None), slice(None, 600)))
+print 'Manipulated data in', time.time() - t_start, 'secs'
+
+t_start = time.time()
+X_PCA = IMDR.fit_transform()
+print 'Fit PCA in ', time.time() - t_start, 'secs'
+
+t_start = time.time()
+#IMDR.reduced_data_to_json(file_name='tmp_json.JSON',
+#                          file_path='/home/david/Desktop/')
+print 'Made JSON in ', time.time() - t_start, 'secs'
 
 
 t_start = time.time()
-X_PCA = IDR.fit_transform()
-print 'Fit PCA in ', time.time() - t_start, 'sec'
-
-t_start = time.time()
-IDR.reduced_data_to_json(file_name='tmp_json.JSON',
-                         file_path='/home/david/Desktop/')
-print 'Dumps JSON in ', time.time() - t_start, 'sec'
-
-
-t_start = time.time()
-IDR.make_thumbnails(
+IMDR.make_thumbnails(
     thumbnail_path='/home/david/Pictures/SAXS_for_DB/thumbnails/',
-    thumbnail_size=(75, 75), thumbnail_type='.jpg')
-print 'Make thumbnails in ', time.time() - t_start, 'sec'
+    thumbnail_size=(75, 75), thumbnail_type='.png')
+print 'Made thumbnails in ', time.time() - t_start, 'secs'
 
 draw_PCA(X_PCA, 1)
